a5daceacc30fbde463e34d4ac4d4eb9d667fad54
Leonardo Schmitt <leonardox.schmitt@gmail.com>
2021-04-05 20:51:49 -0300

    SideBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -18,7 +18,7 @@ export function Header() {
         fontSize='3xl'
         fontWeight='bold'
         letterSpacing='tight'
-        w='64'
+        w='50'
       >
         DashGo
         <Text as='span' ml='1' color='pink.500'>.</Text>
@@ -43,12 +43,43 @@ export function Header() {
           px='4'
           mr='4'
           placeholder='Buscar na plataforma'
-          _placeholder={{color:'gray.400'}}
+          _placeholder={{ color: 'gray.400' }}
 
         />
-        <Icon as={RiSearchLine} fontSize='20'/>
+        <Icon as={RiSearchLine} fontSize='20' />
       </Flex>
 
+
+      <Flex
+        align="center"
+        ml='auto'
+      >
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Leonardo Schmitt</Text>
+            <Text color='gray.300' fontSize='small'>
+              leonardox.schmitt@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size='md' 
+          name='Leonardo Schmitt' 
+          src='https://github.com/Neilom.png' />
+
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
