60d40d7642d5ff8f8d5c1a4139aeaa0b84eafede
Leonardo Schmitt <leonardox.schmitt@gmail.com>
2021-06-29 22:07:28 -0300

    adicionado o react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,23 @@ import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SideBarDrawerContext'
 import { makeServer } from '../services/mirage'
 
+import { QueryClientProvider, QueryClient } from 'react-query'
+
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryCliente = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryCliente}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,17 +6,39 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
+import { useQuery } from 'react-query'
+import { Spinner } from "@chakra-ui/react";
+
 
 export default function UserList() {
+
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        })
+      }
+    })
+
+    return users
+  })
+
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -34,47 +56,60 @@ export default function UserList() {
             </Link>
           </Flex>
 
+          {isLoading ? (
+            <Flex justify='center'> <Spinner /> </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Falha ao carregar</Text>
+            </Flex>
+          ) : (<>
+            <Table colorScheme='whiteAlpha'>
+              <Thead>
+                <Tr>
+                  <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                    <Checkbox colorScheme="green" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  {isWideVersion && <Th>Data de cadastro</Th>}
+                  <Th w='8'></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map(user => {
+                  return (
+                    <Tr key={user.id}>
+                    <Td p={['4', '4', '6']}>
+                      <Checkbox colorScheme="green" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>{user.name}</Text>
+                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>{user.createdAt}</Td>}
+                    <Td >
+                      <Button
+                        as='a'
+                        size='sm'
+                        fontSize='sm'
+                        colorScheme='green'
+                        leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                      >
+                        {isWideVersion ? "Editar" : ""}
+                      </Button>
+                    </Td>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme="green" />
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w='8'></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td p={['4', '4', '6']}>
-                  <Checkbox colorScheme="green" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Leonardo Schmitt</Text>
-                    <Text fontSize='sm' color='gray.300'>leonardox.schmitt@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td >
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='green'
-                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                  >
-                    {isWideVersion ? "Editar" : ""}
-                  </Button>
-                </Td>
+                  </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
 
-              </Tr>
-            </Tbody>
-          </Table>
+            <Pagination />
+          </>
+          )}
 
-          <Pagination />
         </Box>
       </Flex>
     </Box>
